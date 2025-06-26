92d224aa50242b02068ca20a9b189d8faf6aa026
Kaden Benzley <ksbenzley2@gmail.com>
2025-06-26 16:50:15 -0600

    Implemented ChessMove functions and added comments functions

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -12,6 +12,7 @@ public class ChessMove {
     private final ChessPosition endPosition;
     private final ChessPiece.PieceType promotionPiece;
 
+    //Constructor
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
 
