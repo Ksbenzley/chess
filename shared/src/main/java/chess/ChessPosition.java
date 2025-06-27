9efaabe490012a8d10087576488194d0cfee1156
Kaden Benzley <ksbenzley2@gmail.com>
2025-06-26 19:39:13 -0600

    updated hashCode override in ChessPosition to be consistent between classes

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -43,7 +45,7 @@ public class ChessPosition {
 
     @Override
     public int hashCode(){
-        return 31 * row + column;
+        return Objects.hash(row, column);
     }
 
 }
