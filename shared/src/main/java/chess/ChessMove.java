835b948b68ddf2376f3e63b267b77070d13be4b6
Kaden Benzley <ksbenzley2@gmail.com>
2025-06-26 18:22:01 -0600

    Overrode equals and hashCode for ChessMove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -43,5 +45,20 @@ public class ChessMove {
      */
     public ChessPiece.PieceType getPromotionPiece() {
         return this.promotionPiece;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove that = (ChessMove) o;
+        return Objects.equals(startPosition, that.startPosition) &&
+                Objects.equals(endPosition, that.endPosition) &&
+                promotionPiece == that.promotionPiece;
+    }
+
+    @Override
+    public int hashCode(){
+        return  Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
