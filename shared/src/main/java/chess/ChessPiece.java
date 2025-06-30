1f90527e251f8fd1dc7cc8e22e0b8169a2351b5a
Kaden Benzley <ksbenzley2@gmail.com>
2025-06-30 13:29:12 -0600

    implemented squares in ChessBoard which acts as the empty spaces and addPiece. Created a subclass called PMCalculator

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -53,7 +54,7 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        //return new ArrayList<>();
     }
 
     @Override
