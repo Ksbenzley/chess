ff62521febce270219ab20d7b4debb5e271d070d
Kaden Benzley <ksbenzley2@gmail.com>
2025-07-16 22:16:59 -0600

    fixed some written tests

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -17,8 +17,8 @@ public class UserServiceTest {
     @Test
     //create a new user
     public void registerUserPass() throws BadRequestException, AlreadyTakenException{
-        UserService.registerUser("username", "password", "e@mail.com", user, auth);
-        assertTrue(user.checkForUser("username"));
+        UserService.registerUser("player", "password", "e@mail.com", user, auth);
+        assertTrue(user.checkForUser("player"));
     }
 
     @Test
