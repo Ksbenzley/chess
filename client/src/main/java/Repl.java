3ad135cb31d914cf96bd597df6d47320038e66c8
Kaden Benzley <ksbenzley2@gmail.com>
2025-07-29 18:44:48 -0600

    created a REPL and server facade to make logic easier

---
+++ client/src/main/java/Repl.java
@@ -0,0 +1,37 @@
+import exceptions.AlreadyTakenException;
+import exceptions.BadRequestException;
+import exceptions.DataAccessException;
+import exceptions.NotAuthorizedException;
+
+import java.util.Scanner;
+
+public class Repl {
+
+    public State state = State.SIGNEDOUT;
+    public clientRequest clientReq;
+
+    public Repl(String serverUrl) {
+        this.clientReq = new clientRequest(serverUrl);
+    }
+
+    public void run(){
+        System.out.println("Welcome to the Chess Game Interface");
+        System.out.println(clientReq.help());
+        System.out.print(">>> ");
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            //printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = clientReq.evaluate(line);
+                System.out.print(result);
+            } catch (AlreadyTakenException | BadRequestException | NotAuthorizedException e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+    }
+
+}
---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,153 @@
+
+import com.google.gson.JsonObject;
+import exceptions.*;
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import requests.*;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+
+public class ServerFacade {
+
+    public final String serverUrl;
+    public String authToken;
+
+    public ServerFacade(String url) {
+        this.serverUrl = url;
+    }
+
+    public AuthData login(UserData userData) throws NotAuthorizedException{
+        var path = "/session";
+        LoginRequest request = new LoginRequest(userData.username(), userData.password());
+        AuthData data = this.makeRequest("POST", path, request, AuthData.class);
+        authToken = data.authToken();
+        return data;
+    }
+
+    public AuthData register(UserData userData) throws AlreadyTakenException {
+        var path = "/user";
+        RegisterRequest request = new RegisterRequest(userData.username(), userData.password(), userData.email());
+        AuthData data = this.makeRequest("POST", path, request, AuthData.class);
+        authToken = data.authToken();
+        return data;
+    }
+
+    public String logout(String params) throws BadRequestException{
+        var path = "/session";
+        return this.makeRequest("DELETE", path, null, null);
+    }
+
+    public GameData createGame(String name) throws BadRequestException {
+        var path = "/game";
+        CreateRequest newGame = new CreateRequest(name);
+        return this.makeRequest("POST", path, newGame, GameData.class);
+    }
+
+    public class ListGamesResponse {
+        private ArrayList<GameData> games;
+
+        public ArrayList<GameData> getGames() {
+            return games;
+        }
+    }
+
+    public ArrayList listGames() throws BadRequestException {
+        var path = "/game";
+        //ArrayList response;
+        ListGamesResponse response = this.makeRequest("GET", path, null, ListGamesResponse.class);
+        return response.getGames();
+    }
+
+    public void observeGame(int gameNum) throws BadRequestException {
+        var path = "/game";
+        ArrayList games;
+        Object game;
+        games = listGames();
+        game = games.get(gameNum);
+        this.makeRequest("PUT", path, game, null);
+    }
+
+    public void playGame(int gameID, String color) throws BadRequestException {
+        var path = "/game";
+        JoinRequest request = new JoinRequest(color.toUpperCase(), gameID);
+        //make ui here??
+        this.makeRequest("PUT", path, request, null);
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass)
+            throws BadRequestException, NotAuthorizedException, AlreadyTakenException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+            http.addRequestProperty("authorization", authToken);
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            if (responseClass != null) {
+                return readBody(http, responseClass);
+            } else {
+                return null;
+            }
+        } catch (BadRequestException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new BadRequestException(ex.getMessage());
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http)
+            throws IOException, BadRequestException, NotAuthorizedException, AlreadyTakenException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            String error = "Unexpected error";
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    InputStreamReader reader = new InputStreamReader(respErr);
+                    JsonObject json = new Gson().fromJson(reader, JsonObject.class);
+                    if (json.has("message")) {
+                        error = json.get("message").getAsString();
+                    }
+                }
+            }
+            switch (status){
+                case 400 -> throw new BadRequestException(error);
+                case 401 -> throw new NotAuthorizedException(error);
+                case 403 -> throw new AlreadyTakenException(error);
+                default -> throw new BadRequestException(error);
+            }
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
